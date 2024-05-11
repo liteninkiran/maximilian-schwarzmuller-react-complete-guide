be7f791a77ba7caeec8dece9057c4c31b8463c7c
Kiran Anand <liteninkiran@hotmail.com>
2024-05-11 19:55:55 +0100

    048 - Component Composition: The special "children" Prop

--- 03 - React Essentials/src/App.jsx
+++ 03 - React Essentials/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import { CORE_CONCEPTS } from './data';
+import TabButton from './components/TabButton';
 
 function App() {
     return (
         <div>
             <Header />
             <main>
-                <section id="core-concepts">
+                <section id='core-concepts'>
                     <h2>Time to get started!</h2>
                     <ul>
                         <CoreConcept {...CORE_CONCEPTS[0]} />
@@ -15,6 +16,17 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[2]} />
                         <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
+                </section>
+
+                <section id='examples'>
+                    <h2>Examples</h2>
+
+                    <menu>
+                        <TabButton>Components</TabButton>
+                        <TabButton>JSX</TabButton>
+                        <TabButton>Props</TabButton>
+                        <TabButton>State</TabButton>
+                    </menu>
                 </section>
             </main>
         </div>
