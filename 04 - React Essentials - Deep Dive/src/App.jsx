31108b0c9bead451fae4f7021649a40559877d75
Kiran Anand <liteninkiran@hotmail.com>
2024-05-12 14:29:19 +0100

    086 - Reducing State Management & Identifying Unnecessary State

--- 04 - React Essentials - Deep Dive/src/App.jsx
+++ 04 - React Essentials - Deep Dive/src/App.jsx
@@ -3,24 +3,16 @@ import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 import Log from './components/Log';
 
-function App() {
-    const [activePlayer, setActivePlayer] = useState('X');
-    const [gameTurns, setGameTurns] = useState([]);
+const deriveActivePlayer = (gameTurns) => (gameTurns.length > 0 && gameTurns[0].player === 'X' ? 'O' : 'X');
 
+function App() {
+    const [gameTurns, setGameTurns] = useState([]);
+    const activePlayer = deriveActivePlayer(gameTurns);
     const handleSelectSquare = (rowIndex, colIndex) => {
-        setActivePlayer((prev) => prev === 'X' ? 'O' : 'X');
         setGameTurns(prev => {
-            const curr = (prev.length > 0 && prev[0].player === 'X' ? 'O' : 'X');
-            const updatedTurns = [
-                {
-                    square: {
-                        row: rowIndex,
-                        col: colIndex,
-                    },
-                    player: curr,
-                },
-                ...prev,
-            ];
+            const activePlayer = deriveActivePlayer(gameTurns);
+            const square = { row: rowIndex, col: colIndex }
+            const updatedTurns = [{ square, player: activePlayer }, ...prev];
             return updatedTurns;
         });
     }
