4b885b38e2f36fbf51abf662725202ae8db78a2a
Kiran Anand <liteninkiran@hotmail.com>
2024-05-12 14:18:56 +0100

    085 - Sharing State Across Components

--- 04 - React Essentials - Deep Dive/src/components/Log.jsx
+++ 04 - React Essentials - Deep Dive/src/components/Log.jsx
@@ -1,10 +1,14 @@
-export default function Log() {
+export default function Log({ turns }) {
 
     return (
         <ol id='log'>
-            <li>
-                Log
-            </li>
+            {
+                turns.map(turn => (
+                    <li key={`${turn.square.row}-${turn.square.col}`}>
+                        {turn.player} selected {turn.square.row}, {turn.square.col}
+                    </li>
+                ))
+            }
         </ol>
     );
 }
