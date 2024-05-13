938247981aa6015f4398899c355ba39a0ba8acae
Kiran Anand <liteninkiran@hotmail.com>
2024-05-13 12:15:59 +0100

    116 - Creating Reusable Components & Component Combinations

--- 06 - Styling Components/src/components/AuthInputs.jsx
+++ 06 - Styling Components/src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
+import { Button, TextButton } from './Button';
+import { InputWithLabel as Input } from './Input';
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState('');
@@ -24,24 +26,17 @@ export default function AuthInputs() {
     return (
         <div id='auth-inputs'>
             <ControlDiv>
-                <p>
-                    <Label $invalid={emailNotValid}>Email</Label>
-                    <Input
-                        type='email'
-                        $invalid={emailNotValid}
-                        onChange={(event) => handleInputChange('email', event.target.value)}
-                    />
-                </p>
-                <p>
-                    <Label $invalid={passwordNotValid}>Password</Label>
-                    <Input
-                        type='password'
-                        $invalid={passwordNotValid}
-                        onChange={(event) =>
-                            handleInputChange('password', event.target.value)
-                        }
-                    />
-                </p>
+                <Input
+                    label='Email'
+                    $invalid={emailNotValid}
+                    onChange={(event) => handleInputChange('email', event.target.value)}
+                />
+                <Input
+                    label='Password'
+                    $invalid={passwordNotValid}
+                    onChange={(event) => handleInputChange('password', event.target.value)}
+                    type='password'
+                />
             </ControlDiv>
             <div className='actions'>
                 <TextButton type='button'>Create a new account</TextButton>
@@ -56,50 +51,4 @@ const ControlDiv = styled.div`
     flex-direction: column;
     gap: 0.5rem;
     margin-bottom: 1.5rem;
-`;
-
-const Label = styled.label`
-    display: block;
-    margin-bottom: 0.5rem;
-    font-size: 0.75rem;
-    font-weight: 700;
-    letter-spacing: 0.1em;
-    text-transform: uppercase;
-    color: ${({ $invalid }) => $invalid ? '#f87171' : '#6b7280'};
-`;
-
-const Input = styled.input`
-    width: 100%;
-    padding: 0.75rem 1rem;
-    line-height: 1.5;
-    background-color: ${({ $invalid }) => $invalid ? '#fed2d2' : '#d1d5db'};
-    color: ${({ $invalid }) => $invalid ? '#ef4444' : '#374151'};
-    border: 1px solid ${({ $invalid }) => $invalid ? '#f73f3f' : 'transparent'};
-    border-radius: 0.25rem;
-    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = styled.button`
-    padding: 1rem 2rem;
-    font-weight: 600;
-    text-transform: uppercase;
-    border-radius: 0.25rem;
-    color: #1f2937;
-    background-color: #f0b322;
-    border-radius: 6px;
-    border: none;
-
-    &:hover {
-        background-color: #f0920e;
-    }
-    
-`;
-
-const TextButton = styled.button`
-    color: #f0b322;
-    border: none;
-
-    &:hover {
-        color: #f0920e;
-    }    
 `;
