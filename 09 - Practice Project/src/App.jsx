0ea657502241a948ca17d6243df9b6081c62dffa
Kiran Anand <liteninkiran@hotmail.com>
2024-05-14 14:54:37 +0100

    153 - Collecting User Input with Refs & Forwarded Refs

--- 09 - Practice Project/src/App.jsx
+++ 09 - Practice Project/src/App.jsx
@@ -10,12 +10,17 @@ function App() {
     }
     const [projectsState, setProjectsState] = useState(initialState);
     const handleStartAddProject = () => setProjectsState(prev => ({ ...prev, selectedProjectId: null }));
+    const handleAddProject = (data) => {
+        const newProject = { ...data, id: Math.floor(Math.random() * (10000)) }
+        setProjectsState(prev => ({ ...prev, projects: [...prev.projects, newProject] }));
+    }
+    console.log(projectsState);
     return (
         <main className='h-screen my-8 flex gap-8'>
             <ProjectsSidebar onStartAddProject={handleStartAddProject} />
             {
                 projectsState.selectedProjectId === null ? (
-                    <NewProject />
+                    <NewProject onAdd={handleAddProject} />
                 ) : (
                     <NoProjectSelected onStartAddProject={handleStartAddProject} />
                 )
--- 09 - Practice Project/src/components/Input.jsx
+++ 09 - Practice Project/src/components/Input.jsx
@@ -1,16 +1,21 @@
-export default function Input({ isTextArea = false, label, ...props }) {
+import { forwardRef } from 'react';
+
+const InputFn = ({ isTextArea = false, label, ...props }, ref) => {
     const inputClasses = 'w-full p-1 rounded-sm border-b-2 border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
     return (
         <p className='flex flex-col gap-1 my-4'>
             <label className='text-sm font-bold uppercase text-stone-500'>{label}</label>
             {
                 isTextArea ? (
-                    <textarea className={inputClasses} {...props} />
+                    <textarea className={inputClasses} {...props} ref={ref} />
                 ) : (
-                    <input className={inputClasses} {...props} />
+                    <input className={inputClasses} {...props} ref={ref} />
                 )
             }
             
         </p>
     );
 }
+
+const Input = forwardRef(InputFn);
+export default Input;
