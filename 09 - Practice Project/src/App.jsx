74345f7e9de4d7ab49388b5b2e341daedf25e06c
Kiran Anand <liteninkiran@hotmail.com>
2024-05-14 14:30:02 +0100

    152 - Managing State to Switch Between Components

--- 09 - Practice Project/src/App.jsx
+++ 09 - Practice Project/src/App.jsx
@@ -1,12 +1,26 @@
 import NoProjectSelected from './components/NoProjectSelected';
 import NewProject from './components/NewProject';
 import ProjectsSidebar from './components/ProjectsSidebar';
+import { useState } from 'react';
 
 function App() {
+    const initialState = {
+        selectedProjectId: undefined,
+        projects: [],
+    }
+    const [projectsState, setProjectsState] = useState(initialState);
+    const handleStartAddProject = () => setProjectsState(prev => ({ ...prev, selectedProjectId: null }));
     return (
         <main className='h-screen my-8 flex gap-8'>
-            <ProjectsSidebar />
-            <NoProjectSelected />
+            <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+            {
+                projectsState.selectedProjectId === null ? (
+                    <NewProject />
+                ) : (
+                    <NoProjectSelected onStartAddProject={handleStartAddProject} />
+                )
+            }
+            
         </main>
     );
 }
