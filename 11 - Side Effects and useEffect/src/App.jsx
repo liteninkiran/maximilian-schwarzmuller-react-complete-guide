2431c887892bb02c7c2b14e1550a037849ceb870
Kiran Anand <liteninkiran@hotmail.com>
2024-05-15 10:05:04 +0100

    184 - Fixing a Small Bug

--- 11 - Side Effects and useEffect/src/App.jsx
+++ 11 - Side Effects and useEffect/src/App.jsx
@@ -59,7 +59,7 @@ function App() {
 
     return (
         <>
-            <Modal open={modalIsOpen}>
+            <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
                 <DeleteConfirmation
                     onCancel={handleStopRemovePlace}
                     onConfirm={handleRemovePlace}
