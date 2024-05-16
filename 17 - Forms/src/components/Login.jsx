45a4ba85af2b50613be3b1c5a50c427bca50ffbf
Kiran Anand <liteninkiran@hotmail.com>
2024-05-16 20:40:00 +0100

    250 - Handling Form Submission

--- 17 - Forms/src/components/Login.jsx
+++ 17 - Forms/src/components/Login.jsx
@@ -1,6 +1,10 @@
 export default function Login() {
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('Submitted');
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Login</h2>
 
             <div className='control-row'>
