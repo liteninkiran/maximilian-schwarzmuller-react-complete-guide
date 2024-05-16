bc6d1f2dfb6b107f3931877636c34c0edafb962e
Kiran Anand <liteninkiran@hotmail.com>
2024-05-16 12:06:18 +0100

    234 - Handling HTTP Errors

--- 15 - HTTP Requests/src/components/AvailablePlaces.jsx
+++ 15 - HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,20 +1,37 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [availablePlaces, setAvailablePlaces] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     useEffect(() => {
         async function fetchPlaces() {
             setIsLoading(true);
-            const response = await fetch('http://localhost:3000/places');
-            const resData = await response.json();
-            setAvailablePlaces(resData.places);
+
+            try {
+                const response = await fetch('http://localhost:3000/places');
+                const resData = await response.json();
+    
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+    
+                setAvailablePlaces(resData.places);
+            } catch (error) {
+                const message = 'Could not fetch data. Please try again later.';
+                setError({ message: error.message || message });
+            }
             setIsLoading(false);
         }
         fetchPlaces();
     }, []);
+
+    if (error) {
+        return (<Error title='An error occurred' message={error.message} />);
+    }
 
     return (
         <Places
--- 15 - HTTP Requests/src/components/Places.jsx
+++ 15 - HTTP Requests/src/components/Places.jsx
@@ -1,5 +1,5 @@
 export default function Places({ title, places, isLoading, loadingText, fallbackText, onSelectPlace }) {
-    console.log(places);
+    // console.log(places);
     return (
         <section className='places-category'>
             <h2>{title}</h2>
