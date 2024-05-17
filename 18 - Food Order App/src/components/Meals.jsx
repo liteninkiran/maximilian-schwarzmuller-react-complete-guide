ee040301e84c50136bfaed23523165438fb568a4
Kiran Anand <liteninkiran@hotmail.com>
2024-05-17 18:32:51 +0100

    278 - Adding a Custom HTTP Hook & Avoiding Common Errors

--- 18 - Food Order App/src/components/Meals.jsx
+++ 18 - Food Order App/src/components/Meals.jsx
@@ -1,28 +1,23 @@
-import { useEffect, useState } from 'react';
-import MealItem from './MealItem.jsx'
+import MealItem from './MealItem.jsx';
+import useHttp from '../hooks/useHttp.js';
 
-const Meals = () => {
-    const [meals, setMeals] = useState([]);
-    useEffect(() => {
-        const fetchMeals = async () => {
-            const response = await fetch('http://localhost:3000/meals');
-            if (!response.ok) {
-                // TODO...
-            }
-            const data = await response.json();
-            setMeals(data);
-        }
-        fetchMeals();
-    }, []);
+const requestConfig = {};
+
+export default function Meals() {
+    const url = 'http://localhost:3000/meals';
+    const { data: meals, isLoading, error } = useHttp(url, requestConfig, []);
+
+    if (isLoading) {
+        return <p>Fetching meals...</p>;
+    }
+
     return (
         <ul id='meals'>
             {
-                meals.map(meal => (
+                meals.map((meal) => (
                     <MealItem key={meal.id} meal={meal} />
                 ))
             }
         </ul>
     );
 }
-
-export default Meals;
---
+++ 18 - Food Order App/src/hooks/useHttp.js
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const message = 'Something went wrong, failed to send request.';
+
+const sendHttpRequest = async (url, config) => {
+    const response = await fetch(url, config);
+    const resData = await response.json();
+    if (!response.ok) {
+        throw new Error(resData.message || message);
+    }
+    return resData;
+}
+
+const useHttp = (url, config, initialData) => {
+    const [data, setData] = useState(initialData);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const sendRequestFn = async () => {
+        setIsLoading(true);
+        try {
+            const resData = await sendHttpRequest(url, config);
+            setData(resData);
+        } catch (error) {
+            setError(error.message || message);
+        }
+        setIsLoading(false);
+    }
+    const sendRequest = useCallback(sendRequestFn, [url, config]);
+
+    useEffect(() => {
+        const configMethod = config && (config.method === 'GET' || !config.method);
+        if (configMethod || !config) {
+            sendRequest();
+        }
+    }, [sendRequest, config]);
+
+    return { data, isLoading, error, sendRequest };
+}
+
+export default useHttp;
