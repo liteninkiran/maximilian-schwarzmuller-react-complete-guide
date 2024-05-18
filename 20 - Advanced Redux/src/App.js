2d85fe342d6895e9115eeef59a28f871c50360a8
Kiran Anand <liteninkiran@hotmail.com>
2024-05-18 20:45:52 +0100

    313 - Using useEffect with Redux

--- 20 - Advanced Redux/src/App.js
+++ 20 - Advanced Redux/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,16 @@ import Products from './components/Shop/Products';
 
 function App() {
     const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+
+    useEffect(() => {
+        const url = 'https://react-demo-cb134-default-rtdb.europe-west1.firebasedatabase.app/cart.json';
+        const options = {
+            method: 'PUT',
+            body: JSON.stringify(cart),
+        }
+        fetch(url, options);
+    }, [cart]);
 
     return (
         <Layout>
