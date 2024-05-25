ed7dd22e3da8475e412126dd3acc06e1af7ad6cf
Kiran Anand <liteninkiran@hotmail.com>
2024-05-25 12:43:52 +0100

    331 - Navigating Programmatically

--- 21 - Routing/src/pages/Home.js
+++ 21 - Routing/src/pages/Home.js
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 function HomePage() {
+    const navigate = useNavigate();
+    const navigateHandler = () => navigate('/products');
+
     return (
         <>
             <h1>My Home Page</h1>
+            <p>
+                <button onClick={navigateHandler}>Go to PRODUCTS</button>
+            </p>
         </>
     );
 }
