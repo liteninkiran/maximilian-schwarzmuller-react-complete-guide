3ee75933159a78c810df00879794a841d934743d
Kiran Anand <liteninkiran@hotmail.com>
2024-05-25 15:12:25 +0100

    348 - Extracting Error Data & Throwing Responses

--- 21 - Routing Project/frontend/src/pages/Error.js
+++ 21 - Routing Project/frontend/src/pages/Error.js
@@ -1,8 +1,30 @@
+import { useRouteError } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation';
+import PageContent from '../components/PageContent';
+
 const ErrorPage = () => {
+    const error = useRouteError();
+
+    let title = 'An error occurred!';
+    let message = 'Something went wrong!';
+
+    if (error.status === 500) {
+        message = JSON.parse(error.data).message;
+    }
+
+    if (error.status === 404) {
+        title = 'Not found!';
+        message = 'Could not find resource or page.';
+    }
+
     return (
-        <main>
-            <h1>An Error Occurred</h1>
-        </main>
+        <>
+            <MainNavigation />
+            <PageContent title={title}>
+                <p>{message}</p>
+            </PageContent>
+        </>
     );
 }
 
