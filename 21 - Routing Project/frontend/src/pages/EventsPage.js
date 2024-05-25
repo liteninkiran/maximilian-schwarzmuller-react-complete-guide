cc9c565baf6ee449edfa419591f4c3da5848db05
Kiran Anand <liteninkiran@hotmail.com>
2024-05-25 13:51:05 +0100

    337 - Time to Practice: Part 6

--- 21 - Routing Project/frontend/src/pages/EventsPage.js
+++ 21 - Routing Project/frontend/src/pages/EventsPage.js
@@ -1,7 +1,29 @@
+import { Link } from 'react-router-dom';
+
 const EventsPage = () => {
     return (
-        <h1>EventsPage</h1>
+        <>
+
+            <h1>Events Page</h1>
+
+            <ul>
+                {
+                    EVENTS.map(event => (
+                        <li key={event.id}>
+                            <Link className='event-link' to={event.id}>{event.title}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
+
+        </>
     );
 }
 
 export default EventsPage;
+
+const EVENTS = [
+    { id: '1', title: 'Event 1' },
+    { id: '2', title: 'Event 2' },
+    { id: '3', title: 'Event 3' },
+];
