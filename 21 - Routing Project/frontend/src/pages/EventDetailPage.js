f8a3ef55e2f0a97bf8ba1e953b984060349c3c2d
Kiran Anand <liteninkiran@hotmail.com>
2024-05-25 13:54:54 +0100

    337 - Time to Practice: Part 7

--- 21 - Routing Project/frontend/src/pages/EventDetailPage.js
+++ 21 - Routing Project/frontend/src/pages/EventDetailPage.js
@@ -1,6 +1,15 @@
+import { Link, useParams } from 'react-router-dom';
+
 const EventDetailPage = () => {
+    const params = useParams();
     return (
-        <h1>EventDetailPage</h1>
+        <>
+            <h1>Event Details</h1>
+            <p>{params.id}</p>
+            <p>
+                <Link to='..' relative='path' className='event-link'>Back To Events</Link>
+            </p>
+        </>
     );
 }
 
