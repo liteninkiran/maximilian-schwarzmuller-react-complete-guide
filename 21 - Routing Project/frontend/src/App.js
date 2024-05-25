7d2d145e60e76a35c925683545a2c54d2fc8f1a2
Kiran Anand <liteninkiran@hotmail.com>
2024-05-25 14:46:35 +0100

    342 - Where Should loader() Code Be Stored?

--- 21 - Routing Project/frontend/src/App.js
+++ 21 - Routing Project/frontend/src/App.js
@@ -1,28 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
 
-const url = 'http://localhost:8080/events';
-
 const eventRoutes = [
     {
         index: true,
         element: <EventsPage />,
-        loader: async () => {
-            const response = await fetch(url);
-            if (!response.ok) {
-                // ...
-            } else {
-                const data = await response.json();
-                return data.events;
-            }
-        },
+        loader: eventsLoader,
     },
     { path: ':id', element: <EventDetailPage /> },
     { path: 'new', element: <NewEventPage /> },
--- 21 - Routing Project/frontend/src/pages/Events.js
+++ 21 - Routing Project/frontend/src/pages/Events.js
@@ -1,10 +1,24 @@
+import { useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
+const url = 'http://localhost:8080/events';
+
 const EventsPage = () => {
+    const events = useLoaderData();
     return (
-        <EventsList />
+        <EventsList events={events} />
     );
 }
 
 export default EventsPage;
+
+export const loader = async () => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        // ...
+    } else {
+        const data = await response.json();
+        return data.events;
+    }
+}
