1b7eeac049b517ae7c0eeda3f706496d8c79c073
Kiran Anand <liteninkiran@hotmail.com>
2024-05-25 13:38:22 +0100

    337 - Time to Practice: Part 2

--- 21 - Routing Project/frontend/src/App.js
+++ 21 - Routing Project/frontend/src/App.js
@@ -1,17 +1,5 @@
 // Challenge / Exercise
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
@@ -20,8 +8,29 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import EventsPage from './pages/EventsPage';
+import EventDetailPage from './pages/EventDetailPage';
+import NewEventPage from './pages/NewEventPage';
+import EditEventPage from './pages/EditEventPage';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: '/events', element: <EventsPage /> },
+            { path: '/events/:id', element: <EventDetailPage /> },
+            { path: '/events/:id/edit', element: <EditEventPage /> },
+            { path: '/events/new', element: <NewEventPage /> },
+        ],
+    }
+]);
+
 function App() {
-    return <div></div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
