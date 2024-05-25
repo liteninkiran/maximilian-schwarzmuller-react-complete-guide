9fad415c51d116eb4c219c164e4644dad8f624a0
Kiran Anand <liteninkiran@hotmail.com>
2024-05-25 12:49:03 +0100

    332 - Defining & Using Dynamic Routes

--- 21 - Routing/src/pages/Products.js
+++ 21 - Routing/src/pages/Products.js
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
+const PRODUCTS = [
+    { id: '1', title: 'Product 1' },
+    { id: '2', title: 'Product 2' },
+    { id: '3', title: 'Product 3' },
+];
+
 function ProductsPage() {
-    return <h1>The Products Page</h1>;
+    return (
+        <>
+            <h1>The Products Page</h1>
+            <ul>
+                {PRODUCTS.map((prod) => (
+                    <li key={prod.id}>
+                        <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default ProductsPage;
