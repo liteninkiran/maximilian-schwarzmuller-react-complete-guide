b9e7d464e072c4f17d424b1f513dac0a44fec778
Kiran Anand <liteninkiran@hotmail.com>
2024-05-26 16:38:35 +0100

    370 - Validating User Input & Outputting Validation Errors

--- 22 - Authentication/frontend/src/components/AuthForm.js
+++ 22 - Authentication/frontend/src/components/AuthForm.js
@@ -1,14 +1,24 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import { Form, Link, useSearchParams, useActionData, useNavigation } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
+    const data = useActionData();
+    const navigation = useNavigation();
     const [searchParams] = useSearchParams();
     const isLogin = searchParams.get('mode') === 'login';
+    const isSubmitting = navigation.state === 'submitting';
+    const hasErrors = data && data.errors;
+    const keys = hasErrors && Object.values(data.errors);
+    const listItems = hasErrors && keys.map(err => (<li key={err}>{err}</li>));
+    const validationErrors = hasErrors && (<ul>{listItems}</ul>);
+    const validationMessage = hasErrors && (<p>{data.message}</p>);
     return (
         <>
             <Form method='post' className={classes.form}>
                 <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+                {validationErrors}
+                {validationMessage}
                 <p>
                     <label htmlFor='email'>Email</label>
                     <input id='email' type='email' name='email' required />
@@ -21,7 +31,9 @@ function AuthForm() {
                     <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
                         {isLogin ? 'Create new user' : 'Login'}
                     </Link>
-                    <button>Save</button>
+                    <button disabled={isSubmitting}>
+                        {isSubmitting ? 'Submitting...' : 'Save'}
+                    </button>
                 </div>
             </Form>
         </>
