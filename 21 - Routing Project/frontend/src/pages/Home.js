ea0166a4accd1153c1093e9b6716c6b99f33abb9
Kiran Anand <liteninkiran@hotmail.com>
2024-05-26 10:08:02 +0100

    361 - Module Summary

--- 21 - Routing Project/frontend/src/pages/Home.js
+++ 21 - Routing Project/frontend/src/pages/Home.js
@@ -1,6 +1,12 @@
+import PageContent from '../components/PageContent';
+
 const HomePage = () => {
     return (
-        <h1>HomePage</h1>
+        <PageContent title='Welcome'>
+            <p>
+                Browse all of our amazing events
+            </p>
+        </PageContent>
     );
 }
 
