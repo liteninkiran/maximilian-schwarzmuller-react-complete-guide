cb08aeed46d673eb405d0667215f2e2548a2d33c
Kiran Anand <liteninkiran@hotmail.com>
2024-06-02 21:38:42 +0100

    494 - Adding Entry Animations

--- 26 - Animations/src/components/Modal.jsx
+++ 26 - Animations/src/components/Modal.jsx
@@ -1,14 +1,15 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
-    return createPortal(
-        <>
-            <div className='backdrop' onClick={onClose} />
-            <dialog open className='modal'>
-                <h2>{title}</h2>
-                {children}
-            </dialog>
-        </>,
-        document.getElementById('modal')
-    );
+    const initial = { opacity: 0, y: 200 }
+    const animate = { opacity: 1, y: 0 }
+    const modal = (<>
+        <div className='backdrop' onClick={onClose} />
+        <motion.dialog initial={initial} animate={animate} open className='modal'>
+            <h2>{title}</h2>
+            {children}
+        </motion.dialog>
+    </>);
+    return createPortal(modal, document.getElementById('modal'));
 }
