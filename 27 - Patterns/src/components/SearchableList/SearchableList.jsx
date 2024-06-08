50f510a4df7a1123bc79a28e19fbb01b2ef1b51f
Kiran Anand <liteninkiran@hotmail.com>
2024-06-08 15:43:07 +0100

    519 - Working with Debouncing

--- 27 - Patterns/src/components/SearchableList/SearchableList.jsx
+++ 27 - Patterns/src/components/SearchableList/SearchableList.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function SearchableList({ items, children, itemKeyFn }) {
+    const lastChange = useRef();
     const [searchTerm, setSearchTerm] = useState('');
     const searchResults = items.filter(item => {
         const title = item.title?.toLowerCase().includes(searchTerm.toLowerCase());
         const descr = item.description?.toLowerCase().includes(searchTerm.toLowerCase());
         return title || descr;
     });
-    const handleChange = event => setSearchTerm(event.target.value);
+    const handleChange = event => {
+        if (lastChange.current) {
+            clearTimeout(lastChange.current);
+        }
+
+        lastChange.current = setTimeout(() => {
+            lastChange.current = null;
+            setSearchTerm(event.target.value);
+        }, 500);
+    }
 
     return (
         <div className='searchable-list'>
