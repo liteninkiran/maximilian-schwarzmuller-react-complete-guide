eb447053e631610e9446996d87483e58a7d61318
Kiran Anand <liteninkiran@hotmail.com>
2024-06-15 15:30:25 +0100

    544 - Testing Connected Components

--- 29 - Testing/src/components/Greeting.js
+++ 29 - Testing/src/components/Greeting.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import Output from './Output';
+
 const Greeting = () => {
     const [changedText, setChangedText] = useState(false);
     const changeTextHandler = () => setChangedText(true);
@@ -9,9 +11,9 @@ const Greeting = () => {
             <h2>Hello World!</h2>
             {
                 changedText ? (
-                    <p>Changed!</p>
+                    <Output>Changed!</Output>
                 ) : (
-                    <p>It's good to see you!</p>
+                    <Output>It's good to see you!</Output>
                 )
             }
             <button onClick={changeTextHandler}>Change Text!</button>
